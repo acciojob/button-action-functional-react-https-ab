06b4e4aaa06045fc649f9df0a75e00159d9e7714
ab30 <anushkab20021130@gmail.com>
2025-12-05 12:14:58 +0530

    button action2

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
-import ButtonAction from "./ButtonAction";
+import React, { useState } from "react";
+import './../styles/App.css';
 
-function App() {
+const App = () => {
+  const [showPara, setShowPara] = useState(false);
+
   return (
-    <div id="main">
-      {/* // Do not alter the main div */}
-      <ButtonAction />
+    <div>
+      {/* Do not remove this main div */}
+
+      <button
+        id="click"
+        onClick={() => setShowPara(true)}
+      >
+        Click me
+      </button>
+
+      {showPara && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.
+        </p>
+      )}
     </div>
   );
-}
-
+};
 
 export default App;
